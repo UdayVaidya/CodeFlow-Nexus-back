7cc33920b9c5f8af2ef67bd4e19b44cd9c333948
udayvaidya13 <105291488+UdayVaidya@users.noreply.github.com>
2024-08-02 14:46:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static("build"));
-app.use(express.static("public"));
+app.use(express.static("public/images"));
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
